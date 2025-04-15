8605c4c2c07eeb7064f4e98ff0785ea67a660614
Nicholas Rumack <nrumack@gmail.com>
2025-04-16 00:51:01 +0200

    First and final commit I hope

--- src/main/java/com/example/demo/entities/Address.java
+++ src/main/java/com/example/demo/entities/Address.java
@@ -22,14 +22,6 @@ public class Address {
         this.postalCode = postalCode;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getStreet() {
         return street;
     }
--- src/main/java/com/example/demo/entities/Member.java
+++ src/main/java/com/example/demo/entities/Member.java
@@ -1,9 +1,8 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
-import java.util.Date;
+
 
 @Entity
 @Table(name = "memberdb")
@@ -38,10 +37,6 @@ public class Member {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
